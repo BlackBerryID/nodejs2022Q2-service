b4c8778473a39677efed5f526eb46c65d02c3df2
BlackBerryID <iddf1994one@gmail.com>
2022-07-17 23:42:14 +0300

    feat: add .env PORT var

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
-import { ArtistsController } from './artists/artists.controller';
-import { ArtistsService } from './artists/artists.service';
-import { TracksController } from './tracks/tracks.controller';
-import { TracksService } from './tracks/tracks.service';
-import { AlbumsController } from './albums/albums.controller';
-import { AlbumsService } from './albums/albums.service';
-import { FavoritesController } from './favorites/favorites.controller';
-import { FavoritesService } from './favorites/favorites.service';
 import { ArtistsModule } from './artists/artists.module';
 import { AlbumsModule } from './albums/albums.module';
 import { TracksModule } from './tracks/tracks.module';
@@ -18,21 +10,7 @@ import { FavoritesModule } from './favorites/favorites.module';
 
 @Module({
   imports: [ArtistsModule, AlbumsModule, TracksModule, FavoritesModule],
-  controllers: [
-    AppController,
-    UsersController,
-    // ArtistsController,
-    // TracksController,
-    // AlbumsController,
-    // FavoritesController,
-  ],
-  providers: [
-    AppService,
-    UsersService,
-    // ArtistsService,
-    // TracksService,
-    // AlbumsService,
-    // FavoritesService,
-  ],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
