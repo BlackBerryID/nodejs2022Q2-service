8552429fa97f88216e698ddd1f24073bd41916f1
BlackBerryID <iddf1994one@gmail.com>
2022-07-18 22:37:41 +0300

    feat: turn on swagger doc

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+import { parse } from 'yaml';
 
 async function bootstrap() {
   console.log(process.env.PORT);
   const app = await NestFactory.create(AppModule);
+
+  const document = await readFile(
+    join(__dirname, '..', 'doc/api.yaml'),
+    'utf-8',
+  );
+
+  SwaggerModule.setup('doc', app, parse(document));
+
   await app.listen(process.env.PORT || 4000);
 }
+
 bootstrap();
